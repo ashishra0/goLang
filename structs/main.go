e10417e0c291177152f169d48754369339386f44
Ashish Rao <ashishrao2598@gmail.com>
2019-07-20 20:38:15 +0530

    add pointers

--- structs/main.go
+++ structs/main.go
@@ -22,6 +22,25 @@ func main() {
 			email: "jim@office.com",
 		},
 	}
-	fmt.Println("Printing employee information")
+
+	contact := contactInfo{
+		phone: 8806011009,
+		email: "ashisrao@email.com",
+	}
+
+	contactPointer := &contact
+	contactPointer.updatePhone(9822147009)
+
+	jimPointer := &jim
+	jimPointer.updateName("Phil")
+
 	fmt.Println(jim)
+	fmt.Println(contact)
+}
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (pointerToContact *contactInfo) updatePhone(newNo int) {
+	(*pointerToContact).phone = newNo
 }

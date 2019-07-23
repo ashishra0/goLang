510cda79ebb482a5ff2ee8c6237639ccaa220143
Ashish Rao <ashishrao2598@gmail.com>
2019-07-23 12:58:50 +0530

    add http request and io.ReadCloser interface

---
+++ http/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("https://ashishra0.github.io/Portfolio")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+}
